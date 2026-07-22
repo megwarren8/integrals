eac4c9a04ce13c724ba0e2bcd94077eb85fa1fc6
megwarren8 <137943048+megwarren8@users.noreply.github.com>
2026-07-22 08:37:18 -0400

    Update student book (2026-07-22)

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@
  * is not part of the built book; the book renders fully with zero network
  * whether or not this worker ever installs.
  */
-var BOOK_HASH = "f4f19e18a068e0aa";
+var BOOK_HASH = "1d69685324711cd9";
 var CACHE_NAME = "book-" + BOOK_HASH;
 var BOOK_PATHS = ["/", "/index.html", "/Integrals.html"];
 
